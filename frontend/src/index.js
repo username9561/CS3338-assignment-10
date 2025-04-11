35206bb4c423d9ca0fe32f8094de47506d750e0c
JValenzuela1 <Jakeval64@gmail.com>
2025-04-11 01:21:05 -0700

    Final Submission

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // Optional, for styling
 import App from './App'; // Importing the main App component
-import reportWebVitals from './reportWebVitals'; // Optional, for measuring performance
 
 // Rendering the App component inside the div with id="root" from the index.html file
 ReactDOM.render(
@@ -12,5 +10,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// Optional, for measuring performance of your app
-reportWebVitals();
